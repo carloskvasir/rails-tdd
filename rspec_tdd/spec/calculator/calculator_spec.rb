7ce124d46fb8bf278d736481431fd945a379dc64
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-06-04 13:33:17 -0300

    fix robocup errors

--- rspec_tdd/spec/calculator/calculator_spec.rb
+++ rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,13 +1,12 @@
 require 'calculator'
 
-describe Calculator, "Sobre a Calculadora" do
-
+describe Calculator, 'Sobre a Calculadora' do
   context '#div' do
     it 'divide by 0' do
-      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
-      expect{subject.div(3, 0)}.to raise_error("divided by 0")
-      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, "divided by 0")
-      expect{subject.div(3, 0)}.to raise_error(/divided/)
+      expect { subject.div(3, 0) }.to raise_error(ZeroDivisionError)
+      expect { subject.div(3, 0) }.to raise_error('divided by 0')
+      expect { subject.div(3, 0) }.to raise_error(ZeroDivisionError, 'divided by 0')
+      expect { subject.div(3, 0) }.to raise_error(/divided/)
     end
   end
 
