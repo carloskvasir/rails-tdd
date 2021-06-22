0193a45be523d8a12ead2c3ca31f8563c5dc5a06
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-06-22 14:32:11 -0300

    As Null Object

--- rspec_tdd/spec/test_doubles/user_spec.rb
+++ rspec_tdd/spec/test_doubles/user_spec.rb
@@ -6,4 +6,13 @@ describe 'Test Double' do
     puts sut.name
     puts sut.password
   end
+
+  it 'as_null_object' do
+    sut = double('User').as_null_object
+    allow(sut).to receive_messages(name: 'Carlos', age: 27)
+    allow(sut).to receive(:password).and_return('secret')
+    puts sut.name
+    puts sut.password
+    sut.abc
+  end
 end
