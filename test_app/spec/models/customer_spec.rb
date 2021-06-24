525d3b1f5519ec04f41df1295da9cdc74ac79f7b
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-06-24 15:38:34 -0300

    Add/use Faker

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 RSpec.describe 'Customer', type: :model do
   it 'Create a Customer' do
     customer = create(:customer)
-    expect(customer.full_name).to eq('Sr. Carlos Lima')
+    expect(customer.full_name).to start_with('Sr. ')
   end
+
+  it { expect{ create(:customer) }.to change {Customer.all.size}.by(1) }
 end
