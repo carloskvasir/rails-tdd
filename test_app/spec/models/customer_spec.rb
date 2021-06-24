456ecc016e223ba73cd67714ccdf09b7dbf2e397
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-06-24 16:14:32 -0300

    Sobrescrevendo atributos

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -2,9 +2,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Customer', type: :model do
-  it 'Create a Customer' do
+  it '#full_name' do
     customer = create(:customer)
     expect(customer.full_name).to start_with('Sr. ')
+  end
+
+  it '#full_name - Sobrescrevendo Atributo' do
+    customer = create(:customer, name: 'Carlos Lima')
+    expect(customer.full_name).to eq('Sr. Carlos Lima')
   end
 
   it { expect{ create(:customer) }.to change {Customer.all.size}.by(1) }
