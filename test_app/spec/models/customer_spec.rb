530b403c253c5e6a77b6d8d1755b6180387cd757
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-06-25 11:43:24 -0300

    Traits

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -30,5 +30,16 @@ RSpec.describe 'Customer', type: :model do
     expect(customer.name.upcase).to eq(customer.name)
   end
 
+  it 'Cliente Masculino' do
+    customer = create(:customer_male)
+    expect(customer.gender).to eq('M')
+  end
+
+  it 'Cliente Masculino Vip' do
+    customer = create(:customer_male_vip)
+    expect(customer.gender).to eq('M')
+    expect(customer.vip).to be_truthy
+  end
+
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
 end
