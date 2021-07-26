cf8c7105861fabce61776f3808fef82f57c1e673
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-07-26 16:33:29 -0300

    filtrando dados sensíveis

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,8 +1,5 @@
 describe 'HTTParty' do
   it 'content-type', vcr: {cassette_name: 'jsonplaceholder/posts'} do
-#    stub_request(:get, 'https://jsonplaceholder.typicode.com/posts/2')
-#      .to_return(status: 200, body: '', headers: { 'content-type': 'application/json' })
-
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match %r{application/json}
