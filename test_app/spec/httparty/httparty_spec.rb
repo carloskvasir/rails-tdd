1e1541f8f5ebbd8b133c594b73a351c228948ef4
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-07-27 11:16:23 -0300

    Modo de Gravação do VCR

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,7 +1,8 @@
 describe 'HTTParty' do
-  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', record: :new_episodes } do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/5')
     content_type = response.headers['content-type']
     expect(content_type).to match %r{application/json}
   end
 end
+
